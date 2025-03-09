77eec580f4bcc0f3ba80ff8571cd61481e46054c
Artemii-Shlychkov <artemii.shlychkov@student.uni-tuebingen.de>
2025-03-09 17:21:00 +0100

    remove unnecessary files, refactoring

--- utils.py
+++ utils.py
@@ -1,14 +1,13 @@
+import re
+from dataclasses import dataclass
+from pathlib import Path
+
 import numpy as np
-
-
 import plotly.graph_objects as go
 import plotly.subplots as sp
-from plotly.subplots import make_subplots
-from dataclasses import dataclass
 import sklearn.datasets
+from plotly.subplots import make_subplots
 from tensorflow.keras.datasets import mnist
-from pathlib import Path
-import re
 
 from tsne_api import TSNEResult, TSNEResultsWithKNN
 
@@ -20,13 +19,15 @@ class SwissRoll:
     n_samples: int
 
     @classmethod
-    def generate(self, n_samples, noise):
+    def generate(cls, n_samples: int, noise: float) -> "SwissRoll":
         datapoints, labels = sklearn.datasets.make_swiss_roll(
             n_samples=n_samples, noise=noise
         )
         return SwissRoll(datapoints=datapoints, labels=labels, n_samples=n_samples)
 
-    def plot(self, width=None, height=None, title=None):
+    def plot(
+        self, width: int = 600, height: int = 600, title: str | None = None
+    ) -> None:
         plot_swiss_roll(
             self.datapoints,
             self.labels,
@@ -45,13 +46,13 @@ class MNIST:
     labels_test: np.array
 
     @classmethod
-    def generate(self, n_training_samples):
+    def generate(cls, n_training_samples: int) -> "MNIST":
         (data_train, labels_train), (data_test, labels_test) = mnist.load_data()
         data_train = data_train[:n_training_samples]
         labels_train = labels_train[:n_training_samples]
         return MNIST(data_train, labels_train, data_test, labels_test)
 
-    def reshape(self):
+    def reshape(self) -> None:
         self.data_train = self.data_train.reshape(self.data_train.shape[0], -1)
         self.data_test = self.data_test.reshape(self.data_test.shape[0], -1)
 
@@ -64,8 +65,9 @@ def plot_tsne_result(
     labels: np.ndarray,
     additional_title: str = "",
     marker_size: int = 3,
+    *,
     black_template: bool = False,
-):
+) -> None:
     if black_template:
         template = "plotly_dark"
         text_color = "white"
@@ -90,12 +92,12 @@ def plot_tsne_result(
             x=data.embedding[:, 0],
             y=data.embedding[:, 1],
             mode="markers",
-            marker=dict(
-                color=labels,
-                size=marker_size,
-                showscale=False,
-                colorscale="Jet",
-            ),
+            marker={
+                "color": labels,
+                "size": marker_size,
+                "showscale": False,
+                "colorscale": "Jet",
+            },
             name="t-SNE Embedding",
         ),
         row=1,
@@ -112,8 +114,8 @@ def plot_tsne_result(
             x=list(range(len(im_kls_filtered))),
             y=im_kls_filtered,
             mode="markers+lines",
-            marker=dict(size=2, color="blue"),
-            line=dict(color="blue"),
+            marker={"size": 2, "color": "blue"},
+            line={"color": "blue"},
             name="KL Divergence",
         ),
         row=2,
@@ -138,7 +140,7 @@ def plot_tsne_result(
             x=list(range(len(data.im_alphas))),
             y=data.im_alphas,
             mode="lines",
-            line=dict(color="red"),
+            line={"color": "red"},
             name="Alpha Values",
         ),
         row=2,
@@ -164,7 +166,7 @@ def plot_tsne_result(
             x=list(range(len(data.im_alpha_grads))),
             y=data.im_alpha_grads,
             mode="lines",
-            line=dict(color="green"),
+            line={"color": "green"},
             name="Alpha Gradient",
         ),
         row=2,
@@ -195,10 +197,16 @@ def plot_tsne_result(
 
     # Titles and Layout
     fig.update_annotations(
-        font=dict(size=16, color=text_color, family="Courier New, monospace")
+        font={"size": 16, "color": text_color, "family": "Courier New, monospace"}
     )
-    axis_label_style = dict(size=16, color=text_color, family="Courier New, monospace")
-    fig.update_xaxes(title=dict(text="Iterations", font=axis_label_style), row=2, col=2)
+    axis_label_style = {
+        "size": 16,
+        "color": text_color,
+        "family": "Courier New, monospace",
+    }
+    fig.update_xaxes(
+        title={"text": "Iterations", "font": axis_label_style}, row=2, col=2
+    )
     knn_recall = getattr(data, "knn_recall", "N/A")
     title_param_1 = "adaptive" if data.optimization_mode else "fixed"
     title_param_2 = (
@@ -214,15 +222,19 @@ def plot_tsne_result(
     )
 
     fig.update_layout(
-        title=dict(
-            text=(
+        title={
+            "text": (
                 f"{fig_title}{title_param_2}<br>{title_param_3}<br>{additional_title}"
             ),
-            x=0.5,
-            y=0.95,
-            yanchor="top",
-            font=dict(size=16, color=text_color, family="Courier New, monospace"),
-        ),
+            "x": 0.5,
+            "y": 0.95,
+            "yanchor": "top",
+            "font": {
+                "size": 16,
+                "color": text_color,
+                "family": "Courier New, monospace",
+            },
+        },
         template=template,
         height=800,
         width=1000,
@@ -234,8 +246,9 @@ def plot_tsne_result(
 
     fig_path = Path(f"figures/{sanitized_fig_title} (plotly).pdf")
     fig_path.parent.mkdir(parents=True, exist_ok=True)  # Ensure the directory exists
-    # with fig_path.open("wb") as f:
-    #     f.write(fig.to_image(format="pdf"))
+
+    with fig_path.open("wb") as f:
+        f.write(fig.to_image(format="pdf"))
 
     fig.show()
 
@@ -247,10 +260,14 @@ def plot_side_by_side(
     additional_title_1: str = "",
     additional_title_2: str = "",
     marker_size: int = 3,
-    title="",
-):
+    title: str = "",
+) -> None:
     # Extract titles for the embeddings
-    def get_titles(data, knn_recall, additional_title):
+    def get_titles(
+        data: TSNEResult | TSNEResultsWithKNN,
+        knn_recall: float,
+        additional_title: str = "",
+    ) -> str:
         if data.optimization_mode:
             mode = "adaptive"
             params = f"Initial α = {data.initial_alpha:.2f}, α learning rate = {data.alpha_lr}"
@@ -265,7 +282,6 @@ def plot_side_by_side(
             summary = (
                 f"Final Loss = {data.kl_divergence:.2f}, kNN recall = {knn_recall:.2f}"
             )
-        # insert empty string
 
         return f"t-SNE embedding with {mode} α parameter<br>{params}<br>{summary}<br>{additional_title}"
 
@@ -274,8 +290,6 @@ def plot_side_by_side(
 
     title1 = get_titles(data1, knn_recall_data1, additional_title_1)
     title2 = get_titles(data2, knn_recall_data2, additional_title_2)
-
-    axis_label_style = dict(size=20, color="white", family="Courier New, monospace")
 
     # Create subplot layout
     fig = sp.make_subplots(
@@ -302,18 +316,20 @@ def plot_side_by_side(
     # Final iteration for x-axis
     x_ticks = [0, len(data1.im_KLs) // 2, len(data1.im_KLs)]
 
-    def add_embedding(data, col, row, labels):
+    def add_embedding(
+        data: TSNEResult | TSNEResultsWithKNN, col: int, row: int, labels: np.ndarray
+    ) -> None:
         fig.add_trace(
             go.Scatter(
                 x=data.embedding[:, 0],
                 y=data.embedding[:, 1],
                 mode="markers",
-                marker=dict(
-                    color=labels,
-                    size=marker_size,
-                    showscale=False,
-                    colorscale="Rainbow",
-                ),
+                marker={
+                    "color": labels,
+                    "size": marker_size,
+                    "showscale": False,
+                    "colorscale": "Rainbow",
+                },
                 name=f"t-SNE Embedding: {data.dataset_name}",
             ),
             row=row,
@@ -327,8 +343,14 @@ def plot_side_by_side(
         )
 
     def add_metric_plot(
-        data, col, row, metric, name, line_color, hline=None, hline_color=None
-    ):
+        col: int,
+        row: int,
+        metric: list[float],
+        name: str,
+        line_color: str,
+        hline: float | None = None,
+        hline_color: str | None = None,
+    ) -> None:
         # exclude 0 values from metric
         metric = [m for m in metric if m != 0]
 
@@ -337,8 +359,8 @@ def plot_side_by_side(
                 x=list(range(len(metric))),
                 y=metric,
                 mode="markers+lines",
-                marker=dict(size=2, color=line_color),
-                line=dict(color=line_color),
+                marker={"size": 2, "color": line_color},
+                line={"color": line_color},
                 name=name,
             ),
             row=row,
@@ -358,10 +380,11 @@ def plot_side_by_side(
             tickvals=x_ticks,
             ticktext=[str(tick) for tick in x_ticks],
             range=[0, len(metric)],
-            title=dict(
-                text="Iterations", font=dict(size=24, family="Courier New, monospace")
-            )
-            if col == 2 or col == 5
+            title={
+                "text": "Iterations",
+                "font": {"size": 24, "family": "Courier New, monospace"},
+            }
+            if col in (2, 5)
             else None,
             row=row,
             col=col,
@@ -387,7 +410,14 @@ def plot_side_by_side(
         data2, 4, 2, data2.im_KLs, "min 𝓛", "blue", data2.im_KLs[-1], "orange"
     )
     add_metric_plot(
-        data2, 5, 2, data2.im_alphas, "Converged α", "red", data2.im_alphas[-1], "green"
+        data2,
+        5,
+        2,
+        data2.im_alphas,
+        "Converged α",
+        "red",
+        data2.im_alphas[-1],
+        "green",
     )
     add_metric_plot(
         data2, 6, 2, data2.im_alpha_grads, "Zero Gradient", "green", 0, "red"
@@ -397,27 +427,33 @@ def plot_side_by_side(
     if data1.dataset_name == data2.dataset_name and data1.n_samples == data2.n_samples:
         fig_title = f"Comparison of t-SNE embeddings for {data1.dataset_name} with {data1.n_samples} samples"
     else:
-        fig_title = f"Comparison of t-SNE embeddings"
+        fig_title = "Comparison of t-SNE embeddings"
 
     # Layout adjustments
     fig.update_annotations(
-        font=dict(size=20, color="white", family="Courier New, monospace")
+        font={"size": 20, "color": "white", "family": "Courier New, monospace"}
     )
     fig.update_layout(
-        title=dict(
-            text=f"{title}",
-            x=0.5,
-            y=0.99,
-            yanchor="top",
-            font=dict(size=20, color="white", family="Courier New, monospace"),
-        ),
+        title={
+            "text": f"{title}",
+            "x": 0.5,
+            "y": 0.99,
+            "yanchor": "top",
+            "font": {"size": 20, "color": "white", "family": "Courier New, monospace"},
+        },
         template="plotly_dark",
         height=800,
         width=1600,
         showlegend=False,
     )
 
-    # fig.write_image(f"figures/{fig_title} (plotly).pdf")
+    sanitized_fig_title = re.sub(r'[<>:"/\\|?*]', "_", fig_title)
+    fig_path = Path(f"figures/{sanitized_fig_title} (plotly).pdf")
+    fig_path.parent.mkdir(parents=True, exist_ok=True)  # Ensure the directory exists
+
+    with fig_path.open("wb") as f:
+        f.write(fig.to_image(format="pdf"))
+
     fig.show()
 
 
@@ -425,38 +461,14 @@ def plot_swiss_roll(
     sr_points: np.ndarray,
     sr_color: np.ndarray,
     n_samples: int,
-    row=1,
-    col=1,
-    fig=None,
-    title=None,
-    width=800,
-    height=600,
-    marker_size=3,
+    row: int = 1,
+    col: int = 1,
+    fig: go.Figure | None = None,
+    title: str | None = None,
+    width: int = 600,
+    height: int = 600,
+    marker_size: int = 3,
 ) -> go.Figure:
-    """
-    Create a 3D scatter plot of the Swiss Roll dataset using Plotly.
-
-    Parameters
-    ----------
-    sr_points : np.ndarray
-        The Swiss Roll dataset points.
-    sr_color : np.ndarray
-        The colors corresponding to each point.
-    n_samples : int
-        The number of samples in the dataset.
-    row : int, optional
-        The row position in a subplot grid. Default is 1.
-    col : int, optional
-        The column position in a subplot grid. Default is 1.
-    fig : go.Figure, optional
-        An existing figure object to add the Swiss Roll plot into.
-
-    Returns
-    -------
-    go.Figure
-        The Plotly figure object with the Swiss Roll 3D plot.
-    """
-
     # If no existing figure is passed, create a new one
     if fig is None:
         fig = make_subplots(rows=row, cols=col, specs=[[{"type": "scene"}] * col] * row)
@@ -468,43 +480,43 @@ def plot_swiss_roll(
             y=sr_points[:, 1],
             z=sr_points[:, 2],
             mode="markers",
-            marker=dict(
-                size=marker_size,
-                color=sr_color,
-                colorscale="Rainbow",  # Choose a colorscale
-                opacity=0.8,
-            ),
+            marker={
+                "size": marker_size,
+                "color": sr_color,
+                "colorscale": "Rainbow",  # Choose a colorscale
+                "opacity": 0.8,
+            },
         ),
         row=row,
         col=col,
     )
-    # title_font_size = width // 40
+
     # Update the layout for a dark background
     fig.update_layout(
         title="Swiss Roll in Ambient Space" if title is None else title,
         template="plotly_dark",
-        scene=dict(
-            xaxis=dict(title="X"),
-            yaxis=dict(title="Y"),
-            zaxis=dict(title="Z"),
-            camera=dict(
-                eye=dict(x=-1, y=2, z=0.5),  # Camera perspective
-            ),
-        ),
+        scene={
+            "xaxis": {"title": "X"},
+            "yaxis": {"title": "Y"},
+            "zaxis": {"title": "Z"},
+            "camera": {
+                "eye": {"x": -1, "y": 2, "z": 0.5},  # Camera perspective
+            },
+        },
         width=width * col,
         height=height * row,
-        font=dict(family="Courier New, monospace", size=14),
+        font={"family": "Courier New, monospace", "size": 14},
     )
 
     fig.update_xaxes(
         title_text="x",
-        title_font=dict(size=12),  # Set the font size for the x-axis title
-        tickfont=dict(size=10),  # Set the font size for the x-axis tick labels
+        title_font={"size": 12},  # Set the font size for the x-axis title
+        tickfont={"size": 10},  # Set the font size for the x-axis tick labels
     )
     fig.update_yaxes(
         title_text="y",
-        title_font=dict(size=12),  # Set the font size for the y-axis title
-        tickfont=dict(size=10),  # Set the font size for the y-axis tick labels
+        title_font={"size": 12},  # Set the font size for the y-axis title
+        tickfont={"size": 10},  # Set the font size for the y-axis tick labels
     )
 
     # Add a text annotation for the number of samples
@@ -515,7 +527,7 @@ def plot_swiss_roll(
         x=1,
         y=0.05,
         showarrow=False,
-        font=dict(color="white"),
+        font={"color": "white"},
     )
     fig.show()
     return fig
